e028f108366931b786e4ceb60563a48197e6872a
Yoimer <ydrf19@gmail.com>
2017-11-16 19:56:21 -0400

    test-client-request-response-150-no-eeprom-checking

--- ESPWebForm/ESPWebForm.ino
+++ ESPWebForm/ESPWebForm.ino
@@ -52,6 +52,10 @@ const char INDEX_HTML[] =
 "</body>"
 "</html>";
 
+String page = "";
+
+String raw_data = "";
+
 // GPIO#0 is for Adafruit ESP8266 HUZZAH board. Your board LED might be on 13.
 //const int LED_BUILTIN = HIGH;
 
@@ -127,11 +131,10 @@ void handleSubmit()
     DUMPvalue = server.arg("DUMP");
     server.sendHeader("Connection", "close");
     server.sendHeader("Access-Control-Allow-Origin", "*");
-    server.send(200, "text/plain", "Dumping Eeprom\r\n");
 
     uint8_t answer = 0;
     // check empty eeprom
-    answer = sendPICcommand("99 000", "500: 255-", TIMEOUT, 0) || sendPICcommand("99 000", "1000: 255-", TIMEOUT, 0);
+    ////answer = sendPICcommand("99 000", "500: 255-", TIMEOUT, 0) || sendPICcommand("99 000", "1000: 255-", TIMEOUT, 0);
     
     if (answer == 1) {
       Serial.println("eeprom is empty");
@@ -140,8 +143,10 @@ void handleSubmit()
         // show data saved in PIC
         // E is the expected last char
         // it reads every char before getting to
-        // E and displays it on console
+        // E and displays it on console and browser too
         sendPICcommand("99 000", "E", TIMEOUT, 1);
+		page = "<h1>Values saved on eeprom </h1><h3>Raw Data:</h3> <h4>"+raw_data+"</h4>";
+		server.send(200, "text/html", page);
       }
   }
 }
@@ -209,10 +214,10 @@ void writeLED(bool LEDon)
 int8_t sendPICcommand(char* PICcommand, char* expected_answer, unsigned int timeout, int show_response) {
 
   uint8_t x = 0,  answer = 0;
-  char response[100];
+  char response[150];
   unsigned long previous;
 
-  memset(response, '\0', 100);    // Initialize the string
+  memset(response, '\0', 150);    // Initialize the string
 
   delay(100);
 
@@ -244,6 +249,10 @@ int8_t sendPICcommand(char* PICcommand, char* expected_answer, unsigned int time
   if (show_response == 1) {
     Serial.println(response);
     Serial.println(answer);
+	raw_data = "";
+	raw_data = String(response);
+	Serial.println("Printing raw data: ");
+	Serial.println(raw_data);
   }
 
   return answer;
