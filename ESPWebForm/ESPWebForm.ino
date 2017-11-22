0b329bccf68d644feac39957968f76e641d3fa5e
Yoimer <ydrf19@gmail.com>
2017-11-22 19:53:07 -0400

    show-advance-curve-request-on-browser-and-serial

--- ESPWebForm/ESPWebForm.ino
+++ ESPWebForm/ESPWebForm.ino
@@ -394,14 +394,17 @@ void handleGenericArgs() { //Handler
 	String message = "Number of args received:";
 	message += server.args();            //Get number of parameters
 	message += "\n";                            //Add a new line
+	String toPIC = "";
 
 	for (int i = 0; i < server.args(); i++) {
 
 		message += "Arg nº" + (String)i + " –> ";   //Include the current iteration value
 		message += server.argName(i) + ": ";     //Get the name of the parameter
 		message += server.arg(i) + "\n";              //Get the value of the parameter
+		toPIC += server.arg(i) + " ";
 	}
 
+	Serial.println(toPIC);
 	server.send(200, "text/plain", message);       //Response to the HTTP request
 
 }
